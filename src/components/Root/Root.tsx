a93917df9f6f11c1f247b1f60d4d70fcd3271fc0
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2023-10-10 15:32:13 +0630

    refactor[App]: Switch from absolute to relative import

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 
-import { useAuth } from "@/contexts/AuthContext";
+import { useAuth } from "../../contexts/AuthContext";
 
 function Root() {
   const authContext = useAuth();
