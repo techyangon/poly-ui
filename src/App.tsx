db22999b2e884913dbe374a563aaf089a2086249
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2023-11-05 15:39:57 +0630

    feat[App]: Add base and login routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import {
   Experimental_CssVarsProvider as CssVarProvider,
   StyledEngineProvider,
 } from "@mui/material/styles";
 
+import Login from "./components/Login/Login";
+import Root from "./components/Root/Root";
+import { AuthProvider } from "./contexts/AuthContext";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 const App = () => {
   return (
     <StyledEngineProvider injectFirst={true}>
-      <CssVarProvider></CssVarProvider>
+      <CssVarProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </CssVarProvider>
     </StyledEngineProvider>
   );
 };
