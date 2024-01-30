3241978cd1c404871db8c4de1285ceba8be803a9
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-01-30 14:42:25 +0630

    refactor[Input]: Create different inputs based on nature of display

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -5,63 +5,34 @@ import {
   useController,
 } from "react-hook-form";
 
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { FilledInputProps } from "@mui/material/FilledInput";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
 
 import styles from "./input.module.scss";
 
-interface InputProps<T extends FieldValues> extends UseControllerProps<T> {
+export interface InputProps<T extends FieldValues>
+  extends UseControllerProps<T> {
   control: Control<T>;
-  readonly?: boolean;
-  type: string;
 }
 
-function Input<T extends FieldValues>({
-  control,
-  name,
-  readonly,
-  type,
-}: InputProps<T>) {
+function Input<T extends FieldValues>({ control, name }: InputProps<T>) {
   const { field, fieldState } = useController({
     control: control,
     name: name,
   });
 
-  const generateInputProps = (): Partial<FilledInputProps> => {
-    if (readonly) {
-      return {
-        endAdornment: (
-          <InputAdornment position="end">
-            <IconButton edge="end">
-              <LockOutlinedIcon />
-            </IconButton>
-          </InputAdornment>
-        ),
-        readOnly: readonly,
-      };
-    }
-    return {};
-  };
-
   return (
     <TextField
-      InputProps={generateInputProps()}
       aria-label={field.name}
-      className={readonly ? styles.readonlyField : styles.inputField}
+      className={styles.inputField}
       error={fieldState.invalid}
       fullWidth={true}
-      helperText={
-        readonly ? "" : fieldState.error ? fieldState.error.message : " "
-      }
+      helperText={fieldState.error ? fieldState.error.message : " "}
       id={field.name}
       hiddenLabel={true}
       name={field.name}
       onChange={field.onChange}
       size="small"
-      type={type}
+      type="text"
       value={field.value}
       variant="filled"
     />
