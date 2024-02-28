bd96c96db5a145e139f0bf101a7dc6c76ca4afe4
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-02-28 11:04:56 +0630

    build[NPM]: Replace Prettier with Biome

--- src/stores/createPermissionsSlice.ts
+++ src/stores/createPermissionsSlice.ts
@@ -2,24 +2,20 @@ import { StateCreator } from "zustand";
 
 import type { PermissionsSlice, StoreState } from "./types";
 
-const usePermissionsStore: StateCreator<
-  StoreState,
-  [],
-  [],
-  PermissionsSlice
-> = (set) => ({
-  permissions: {
-    audit: [],
-    branches: [],
-    dashboard: [],
-    locations: [],
-    roles: [],
-    resources: [],
-    schedules: [],
-    staff: [],
-    students: [],
-  },
-  updatePermissions: (permissions) => set({ permissions: permissions }),
-});
+const usePermissionsStore: StateCreator<StoreState, [], [], PermissionsSlice> =
+  (set) => ({
+    permissions: {
+      audit: [],
+      branches: [],
+      dashboard: [],
+      locations: [],
+      roles: [],
+      resources: [],
+      schedules: [],
+      staff: [],
+      students: [],
+    },
+    updatePermissions: (permissions) => set({ permissions: permissions }),
+  });
 
 export default usePermissionsStore;
